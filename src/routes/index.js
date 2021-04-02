839d5f9253d19bcda4a6b5e4748ebcac20b1e7d9
ismailaissami <aissami.ismail@gmail.com>
2021-04-02 21:48:06 +0200

    subscriptions crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const usersRouter = require('./users.route');
+const subscriptionsRouter = require('./subscriptions.route');
+
 router.use(usersRouter);
+router.use(subscriptionsRouter);
+
 
 module.exports = router;
